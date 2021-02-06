1256dbe622f7e4e270cf6770b122fa9679b6fdb0
Sara <65115598+codeforfun-jp@users.noreply.github.com>
2021-02-06 14:02:42 +0900

    first commit

---
+++ Android Studio 4.1/app/src/main/java/com/example/catchtheball/ResultActivity.java
@@ -0,0 +1,48 @@
+package com.example.catchtheball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView scoreLabel = findViewById(R.id.scoreLabel);
+        TextView highScoreLabel = findViewById(R.id.highScoreLabel);
+
+        int score = getIntent().getIntExtra("SCORE", 0);
+        //scoreLabel.setText(score + "");
+        scoreLabel.setText(getString(R.string.result_score, score));
+
+        SharedPreferences sharedPreferences = getSharedPreferences("GAME_DATA", MODE_PRIVATE);
+        int highScore = sharedPreferences.getInt("HIGH_SCORE", 0);
+
+        if (score > highScore) {
+            //highScoreLabel.setText("High Score : " + score);
+            highScoreLabel.setText(getString(R.string.result_high_score, score));
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("HIGH_SCORE", score);
+            editor.apply();
+
+        } else {
+            //highScoreLabel.setText("High Score : " + highScore);
+            highScoreLabel.setText(getString(R.string.result_high_score, highScore));
+        }
+    }
+
+    public void tryAgain(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    @Override
+    public void onBackPressed() { }
+}
