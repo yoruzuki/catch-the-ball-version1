5418665827a02dea10229ab71b532769c69d3739
yoruzuki <yoruzuki1@gmail.com>
2021-04-29 15:29:21 +0900

    GIT test

--- Android Studio 4.1/app/src/main/java/com/example/catchtheball/MainActivity.java
+++ Android Studio 4.1/app/src/main/java/com/example/catchtheball/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         //scoreLabel.setText("Score : 0");
         scoreLabel.setText(getString(R.string.score, 0));
 
-        Log.v("TEST", "HelloWorld！");
+        Log.v("TEST11", "HelloWorld！");
         Log.v("SIZE", "onCreate boxY: " + box.getY());
     }
 
